ec83cf3899ccd4c17b5aa3d72ece9932f2e77fcc
Yuto5176 <52201217+Yuto5176@users.noreply.github.com>
2020-12-13 18:43:25 +0900

    add

---
+++ AmabieProject/app/src/main/java/com/kobito19/amabieproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kobito19.amabieproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
