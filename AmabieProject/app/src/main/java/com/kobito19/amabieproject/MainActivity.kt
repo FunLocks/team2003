2ef0178ea1ca1c6bb6bff234bea3a34ca3dcc293
Yuto5176 <52201217+Yuto5176@users.noreply.github.com>
2020-12-14 01:27:41 +0900

    add share =_button

--- AmabieProject/app/src/main/java/com/kobito19/amabieproject/MainActivity.kt
+++ AmabieProject/app/src/main/java/com/kobito19/amabieproject/MainActivity.kt
@@ -2,10 +2,19 @@ package com.kobito19.amabieproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var text_console = findViewById<TextView>(R.id.text_message)
+        var text_share = findViewById<TextView>(R.id.share_button)
+
+        text_share.setOnClickListener{
+            text_console.setText(R.string.text_message1)
+        }
     }
 }
