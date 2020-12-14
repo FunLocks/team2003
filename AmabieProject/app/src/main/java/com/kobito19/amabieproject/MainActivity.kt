18af4129f2312aef70c8462a4875cce5f64df83b
Yuto5176 <52201217+Yuto5176@users.noreply.github.com>
2020-12-15 02:06:42 +0900

    test

--- AmabieProject/app/src/main/java/com/kobito19/amabieproject/MainActivity.kt
+++ AmabieProject/app/src/main/java/com/kobito19/amabieproject/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.core.app.ShareCompat
-
+//test
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
