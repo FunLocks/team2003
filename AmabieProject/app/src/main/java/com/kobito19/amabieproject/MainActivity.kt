5b0b6c3897e21c6b23e551070896ef2ed879911e
shu-holly <shujp0101@gmail.com>
2020-12-14 10:49:34 +0900

    add pernussion dialog

--- AmabieProject/app/src/main/java/com/kobito19/amabieproject/MainActivity.kt
+++ AmabieProject/app/src/main/java/com/kobito19/amabieproject/MainActivity.kt
@@ -1,14 +1,42 @@
 package com.kobito19.amabieproject
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_CODE_LOCATION = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "許可されています。", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "許可されました。", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "拒否されました。", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     fun onClickSetting(view: View) {
