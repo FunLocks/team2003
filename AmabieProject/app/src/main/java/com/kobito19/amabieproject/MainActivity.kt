8b7f829fa63d31ccc9f6281b450532ba6b6bb3d8
Yuto5176 <52201217+Yuto5176@users.noreply.github.com>
2020-12-17 04:59:36 +0900

    add img

--- AmabieProject/app/src/main/java/com/kobito19/amabieproject/MainActivity.kt
+++ AmabieProject/app/src/main/java/com/kobito19/amabieproject/MainActivity.kt
@@ -1,24 +1,62 @@
 package com.kobito19.amabieproject
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.core.app.ShareCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.FileOutputStream
+
 //test
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val decorView: View = window.decorView
         var text_share = findViewById<TextView>(R.id.share_button)
 
         text_share.setOnClickListener{
-            val builder = ShareCompat.IntentBuilder.from(this)
-            builder.setText("Test")
-            builder.setSubject("")
-            builder.setType("text/plain")
-            builder.startChooser()
+            share(decorView, "test")
+//            val builder = ShareCompat.IntentBuilder.from(this)
+//            builder.setText("Test")
+//            builder.setSubject("")
+//            builder.setType("text/plain")
+//            builder.startChooser()
         }
+    }
+    // シェア用のUriを取得
+    private fun Activity.getViewCacheContentUri(view: View): Uri {
+        view.isDrawingCacheEnabled = true
+        view.destroyDrawingCache()
+        val cache = view.drawingCache
+        val bitmap = Bitmap.createBitmap(cache)
+        view.isDrawingCacheEnabled = false
+        val cachePath = File(cacheDir, "images")
+        cachePath.mkdirs()
+        val filePath = File(cachePath, "share.png")
+        val fos = FileOutputStream(filePath.absolutePath)
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
+        fos.close()
+        return FileProvider.getUriForFile(this, "$packageName.fileprovider", filePath)
+    }
+
+    // シェア
+    fun Activity.share(view: View, shareText: String) {
+        val contentUri = getViewCacheContentUri(view)
+        startActivity(Intent().apply {
+            action = Intent.ACTION_SEND
+            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+            setDataAndType(contentUri, contentResolver.getType(contentUri))
+            putExtra(Intent.EXTRA_STREAM, contentUri)
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, shareText)
+        })
     }
 }
